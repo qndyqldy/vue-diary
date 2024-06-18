6a99fa9dafc6222340edf3d0bc47f65153d28ffc
임영욱 <ywyim@gaia3d.com>
2024-06-18 18:44:57 +0900

    chore : pinia 추가

--- vue-diary/src/main.js
+++ vue-diary/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routes/router.js';
+import {createPinia} from 'pinia';
 
+const pinia = createPinia();
 
 createApp(App)
+	.use(pinia)
 	.use(router)
 	.mount('#app')
