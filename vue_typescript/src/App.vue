85781214190aaf442da884d72d3607550c7e5417
임영욱 <ywyim@gaia3d.com>
2024-06-20 12:20:19 +0900

    feat : vue + typescript 정리 프로젝트 생성 및 추가

---
+++ vue_typescript/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import {ref, Ref} from "vue";
+
+type Dog = {
+    name: string;
+    age: number;
+}
+const dog: Ref<Dog> = ref({
+    name: '황구',
+    age: 10
+});
+
+const onClick = (id: number, age: number, title: string) => {
+    console.log(`${id} - ${age} - ${title}`);
+}
+</script>
+
+<template>
+
+  <HelloWorld :dog="dog" title="제목" :count="30" @on-click="onClick"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vue_typescript/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+import {Reactive, reactive, ref, Ref} from "vue";
+
+    const num: Ref<number> = ref(0);
+
+    type Dog = {
+        name: string;
+        age: number;
+    }
+    const dog: Ref<Dog> = ref({
+        name: '황구',
+        age: 10
+    });
+
+    interface Props {
+        title: string;
+        dog: Dog;
+        count: number;
+    }
+
+    const props = defineProps<Props>();
+
+    const emits = defineEmits<{
+        (e: 'onClick', id: number, age: number, title: string): void;
+    }>();
+
+    const onclick = () => {
+        emits('onClick', 0, 20, '제목');
+    }
+
+    const onChange = (e: Event) => {
+        const target = e.target as HTMLInputElement;
+        console.log(target.value);
+    }
+</script>
+
+<template>
+    {{props.title}}
+    {{props.dog}}
+    {{props.count}}
+
+    <button @click="onclick">클릭</button>
+    <input type="text" @change="onChange" />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
